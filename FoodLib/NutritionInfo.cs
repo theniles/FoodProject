b6bd938db3f5ac29f1bb521c32284c31f25d1313
theniles <63492838+theniles@users.noreply.github.com>
2022-02-02 22:34:11 +0200

    Add project files.

---
+++ FoodLib/NutritionInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodLib
+{
+    public struct NutritionInfo
+    {
+        public double Protein { get; set; }
+        public double Lipids { get; set; }
+        public double Carbohydrates { get; set; }
+        public double Calories { get; set; }
+
+        public NutritionInfo(double protein, double lipids, double carbohydrates, double calories)
+        {
+            Protein = protein;
+            Lipids = lipids;
+            Carbohydrates = carbohydrates;
+            Calories = calories;
+        }
+    }
+}
---
+++ FoodSharp/NutritionInfoDisplay.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FoodSharp
+{
+    /// <summary>
+    /// Interaction logic for NutritionInfoDisplay.xaml
+    /// </summary>
+    public partial class NutritionInfoDisplay : UserControl
+    {
+        public NutritionInfoDisplay()
+        {
+            InitializeComponent();
+
+            this.DataContext = new FoodLib.NutritionInfo(10, 20, 15, 350);
+        }
+    }
+}
